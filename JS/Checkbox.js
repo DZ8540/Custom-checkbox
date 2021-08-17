fc1caf9f9b06e5d63de8d13d89a3018c0bde463f
8DZ8 <d.z.2021@inbox.ru>
2021-08-17 23:21:44 +0300

    Added multiple checkboxes

---
+++ JS/Checkbox.js
@@ -0,0 +1,39 @@
+class Checkbox {
+  toggleClass = 'Checkbox__fill--active';
+
+  constructor(item) {
+    this.item = item;
+    this.input = item.querySelector('input');
+    this.checkbox = item.querySelector('[data-id="checkboxInput"]');
+    this.status = this.item.dataset.checked ?? false;
+
+    this.handle();
+  }
+
+  handle() {
+    this.check();
+    this.item.onclick = this.click.bind(this);
+  }
+
+  click() {
+    this.status = !this.status;
+    this.check();
+  }
+
+  check() {
+    if (this.status)
+      this.add();
+    else
+      this.remove();
+  }
+
+  add() {
+    this.checkbox.classList.add(this.toggleClass);
+    this.input.checked = this.status;
+  }
+
+  remove() {
+    this.checkbox.classList.remove(this.toggleClass);
+    this.input.checked = this.status;
+  }
+}
--- JS/index.js
+++ JS/index.js
@@ -1,37 +1,5 @@
-let toggleClass = 'Checkbox__fill--active';
-
 let checkbox = document.querySelector('#checkbox');
-let input = document.querySelector('#input');
-let checkboxInput = document.querySelector('#checkboxFill');
-let state = {
-  input,
-  checkbox,
-  checkboxInput,
-  toggleClass,
-  status: this.input.checked,
-  click() {
-    this.status = !this.status;
-    if (this.status)
-      this.add();
-    else
-      this.remove();
-  },
-  add() {
-    this.checkboxInput.classList.add(this.toggleClass);
-    this.input.checked = this.status;
-  },
-  remove() {
-    this.checkboxInput.classList.remove(this.toggleClass);
-    this.input.checked = this.status;
-  },
-  handle() {
-    if (this.status)
-      this.add();
-    else
-      this.remove();
+new Checkbox(checkbox);
 
-    this.checkbox.onclick = this.click.bind(this);
-  }
-}
-
-state.handle();
+let checkbox2 = document.querySelector('#checkbox2');
+new Checkbox(checkbox2);
